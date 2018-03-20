32e58a66396246ae98214a278e72c3aa3a332e5e
Faith Walker <faithcorinne@gmail.com>
2018-03-20 00:13:27 -0400

    Updating name of directory to correspond with updated README file

---
+++ learn-python-side-by-side/basicmath.py
@@ -0,0 +1,63 @@
+# Exercise - Arithmetic Operator
+# The goal of this exercise is to become familiar with the most common Python operators
+
+# 1. Add 5 + 6
+print('5 + 6 = ' + str(5 + 6))
+
+# 2. Subtract 16 and 5
+print('16 - 5 =' + str(16 - 5))
+
+# 3. Multiply 5 by 3
+print('5 * 3 = ' + str(5 * 3))
+
+# 4. Divide 13 by 2
+print('13 / 2 = ' + str(13 / 2))
+
+# 5. Floor division 13 by 2
+print('13 // 2 = ' + str(13 // 2))
+
+# 6. Modulus 19 by 7
+print('19 % 7 = ' + str(19 % 7))
+
+# Exercise - Comparison Operator
+# The goal of this exercise is to become comfortable with comparison operators
+
+# 1. 3 is greater than 21
+print('The statement 3 > 21 is ' + str(3 > 21))
+
+# 2. 4 is greater than or equal to 12
+print('The statement 4 >= 12 is ' + str(4 >= 12))
+
+# 3. 40 is lesser than 221
+print('The statement 40 < 221 is ' + str(40 < 221))
+
+# 4. 12 is lesser than or equal to 13
+print('The statement 12 <= 13 is ' + str(12 <= 13))
+
+# 5. 26 is equal to 26
+print('The statement 26 == 26 is ' + str(26 == 26))
+
+# 6. 5 is not equal to 12
+print('The statement 5 != 12 is ' + str(5 != 12))
+
+# Exercise - Logical operators
+# The goal of this exercise is to become familiarized with logical operators in the context of Python
+
+# 1. (3 is greater than 21) and (56 is equal to 23)
+print('The statement (3 > 21) AND (56 == 23) is ' + str((3 > 21) and (56 == 23)))
+
+# 2. (4 is greater than or equal to 12) or (43 is lesser than or equal to 122)
+print('The statement (4 >= 12) OR (43 <= 122) is ' + str((4 >= 12) or (43 <= 122)))
+
+# 3. (40 is lesser than 221) and (34 is not equal to 34)
+print('The statement (40 < 221) AND (34 != 34) is ' + str((40 < 221) and (34 != 34)))
+
+
+# 4. (12 is lesser than or equal to 13) and (54 is greater than 23)
+print('The statement (12 <= 13) AND (54 > 23) is ' + str((12 < 13) and (54 > 23)))
+
+# 5. (26 is equal to 26) or (89 is not equal to 44)
+print('The statement (26 == 26) OR (89 != 44) is ' + str((26 == 26) or (89 != 44)))
+
+# 6. (5 is not equal to 12) and (67 is lesser than 88)
+print('The statement (5 != 12) AND (67 < 88) is ' + str((5 != 12) and (67 < 88)))
