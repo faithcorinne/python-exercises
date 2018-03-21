0ecef6ec93b291cc703461dfb34f71138d660b8f
Faith Walker <faithcorinne@gmail.com>
2018-03-21 19:21:57 -0400

    Uploading variables practice exercises

---
+++ learn-python-side-by-side/variablesprac.py
@@ -0,0 +1,26 @@
+# Variables Exercise
+# The goal of this exercise is to become familar with Python variables (mostly naming conventions)
+
+# 1. Create a variable named var1 and assign multiplication of 2 and 3 to it to it
+twotimesthree = 2*3
+print('The variable twotimesthree is assigned the value ' + str(twotimesthree))
+
+# 2. Create a variable named num and assign any odd number to it with decimals (float)
+num = 7.00
+print('The variable num is assigned the value ' + str(num))
+
+# 3. Create a variable named "name" and assign your name to it
+name = 'Faith'
+print('The variable name is assigned the value ' + str(name))
+
+# 4. Create a variable named "age" and assign your age to it (type int)
+age = 24
+print('The variable age is assigned the value ' + str(age))
+
+# 5. Create a variable named "item price" and assign a number to it (type str)
+item_price = 5
+print('The variable item_price is assigned the value ' + str(item_price))
+
+# 6. Create a variable named item2 and assign a value to it (type int)
+item2 = 18
+print('The variable item2 is assigned the value ' + str(item2))
